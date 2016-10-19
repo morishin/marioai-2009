c06bfa48b245fcd423fdfd8e0c8bdf3b5f2d1da6
Shintaro Morikawa <sntr92@gmail.com>
2016-10-19 22:50:16 +0900

    Implement crossover

--- src/python/competition/ga/controller.py
+++ src/python/competition/ga/controller.py
@@ -6,5 +6,18 @@ class Controller(object):
         return numpy.random.choice(individuals, size=2, replace=False, p=probabilities)
 
     @classmethod
-    def cross(cls):
-        pass
+    def two_points_cross(cls, individual1, individual2, indices=None):
+        if indices is None:
+            max_index = min(len(individual1.data), len(individual2.data))
+            index1 = numpy.random.randint(0, max_index)
+            index2 = numpy.random.randint(index1, max_index)
+            indices = (index1, index2)
+
+        child1 = numpy.hstack((individual1.data[:indices[0]],
+            individual2.data[indices[0]:indices[1]],
+            individual1.data[indices[1]:]))
+        child2 = numpy.hstack((individual2.data[:indices[0]],
+            individual1.data[indices[0]:indices[1]],
+            individual2.data[indices[1]:]))
+
+        return (child1, child2)
